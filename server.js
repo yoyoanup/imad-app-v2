320e36adff93b8779f047b118023bb1a506d4ab4
yoyoanup <nanki.anup@gmail.com>
2017-02-24 10:12:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/srticle-one', function (req,res) {
+    res.send('article one requested and it will be served');
+});
+
+app.get('/srticle-two', function (req,res) {
+    res.send('article two requested and it will be served');
+});
+
+app.get('/srticle-three', function (req,res) {
+    res.send('article three requested and it will be served');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
