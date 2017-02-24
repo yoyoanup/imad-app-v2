8bde833656656ed3ac8675831110b27a92af85a4
yoyoanup <nanki.anup@gmail.com>
2017-02-24 11:06:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,17 +5,12 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 
 app.get('/article-one', function (req,res) {
     res.send('article one requested and it will be served');
