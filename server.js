81a9a4660a072322d63e77582f8e64d10e5b28b0
yoyoanup <nanki.anup@gmail.com>
2017-02-24 11:09:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+});
 
 
 app.get('/article-one', function (req,res) {
