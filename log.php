222766f978f9b733c1a0797a2bdba8d30e5a499e
yoyoanup <nanki.anup@gmail.com>
2017-03-19 14:55:14 +0530

    Create log.php

---
+++ log.php
@@ -0,0 +1,23 @@
+  <html>
+           <head>
+		   <style>.a
+		   {
+			   text-align: center;
+			   font-size: 100px;
+			   color:red;
+		   }
+		   </style>
+		   <head>
+		   <body bgcolor="green">
+		   <div class="a">
+		   <form method="post" action="index.php">
+		   username <input type="text" name="username"><br>
+		   password<input type="text" name="password"><br>
+		   <input type="submit" name="submit">
+		   </form>
+		   </div>
+		   
+		   <a href="b.php">signup</a>
+		   </body>
+ 
+ </html>
