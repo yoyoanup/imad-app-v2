6b16dd513a5e880ab04a5c7b7fce7917fb9d298c
yoyoanup <nanki.anup@gmail.com>
2017-03-29 20:04:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,9 +11,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'log.php'));
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'b.php'));
-});
+
 
 
 app.get('/ui/style.css', function (req, res) {
