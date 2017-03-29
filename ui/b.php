231c97c3ce8fb4dfe69f2afa0084a50bcace4afb
yoyoanup <nanki.anup@gmail.com>
2017-03-29 19:53:39 +0530

    Create b.php

---
+++ ui/b.php
@@ -0,0 +1,12 @@
+<html>
+     <body>
+	 <form action="c.php"   method="post">
+	            username <input type="text" name="username"><br>
+		        password   <input type="text" name="password"><br>
+				<input type="reset" name="reset">
+	 <input type="submit" name="submit">
+	 </form>
+     </body>
+
+
+</html>
